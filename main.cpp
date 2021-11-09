25ad630ace077871bd7ae7e63fd3ff90c2f96165
Pawel Cetnarowski <student.pawel2000@gmail.com>
2021-11-09 14:48:37 +0100

    Wersja 1.2

--- main.cpp
+++ main.cpp
@@ -5,21 +5,13 @@ using namespace std;
 
 int main(){
 	
-	//  ver 1.0
+	//  ver 1.2
 	
 	double a,b,x0;
 	
 	
 	cout << "Podaj wspolczynnik a: ";
 	cin >> a;
-	if (a==0){
-		cout << "Wspolczynnik a musi byc rozny od 0" << endl;
-		cout << "Wprowadz a: ";
-		cin>>a;
-	}
-   
-	
-	
 	
 	cout << "Podaj wspolczynnik b: ";
 	cin >> b;
