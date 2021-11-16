1e531505fa172a18f4eddde2a89ea245ab18cfa3
Piotr Bożek <piotrbozek2000@gmail.com>
2021-11-16 14:52:11 +0100

    ver1.4

--- main.cpp
+++ main.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
 using namespace std;
 
-
-
 int main(){
 	
-	//  ver 1.2
+	//  ver 1.4
 	
-	double a,b,x0;
+	double a,b,x0,y,c;
 	
-	
-	cout << "Podaj wspolczynnik a: ";
-	cin >> a;
-	
-	cout << "Podaj wspolczynnik b: ";
-	cin >> b;
-	
-
+	cout << "Podaj wspolczynnik a: "; cin >> a;	
+	cout << "Podaj wspolczynnik b: "; cin >> b;
+	cout << "Podaj wspolczynnik y: "; cin >> y;
+	cout << "Podaj wspolczynnik c: "; cin >> c;
 		
-		x0=-(b/a); 
+		x0=-(b*y-c)/a; 
 		
 		cout << "Miejsce zerowe funkcji to: " << x0; 
-	
-// Aktualizacja / test Piod /test v100
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		
 	return 0;
 }
