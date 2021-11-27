a76393290f8765cfed0890510df0ddec09217bbb
Piotr Bożek <piotrbozek2000@gmail.com>
2021-11-27 15:07:18 +0100

    update ver 1.4

--- main.cpp
+++ main.cpp
@@ -4,19 +4,25 @@ using namespace std;
 
 int main(){
 	
+	cout << "Postac kanoniczna"<<endl<<endl;
 	//  ver 1.4
 	
 	double x0,x1,p,q,a;
 	
 	cout << "Podaj wspolczynnik p: "; cin >> p;	
 	cout << "Podaj wspolczynnik q: "; cin >> q;
+	 if(q > 0){
+	 
+    cout<<"Niema takiej funkcji kwadratowej  ";
+	exit(0); 
+	}//sprawdzanie warunku a
 	cout << "Podaj wspolczynnik a: "; cin >> a;
 
 		
 		x0=p+(sqrt(-q/a)); 
 		x1=p-(sqrt(-q/a));
 		
-		cout << "Miejsce zerowe funkcji to: " << x0; 
+		cout << "Miejsce zerowe funkcji to: " << x0 <<endl; 
 		cout << "Miejsce zerowe funkcji to: " << x1; 
 		
 	return 0;
