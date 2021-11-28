5e140a04e741e30cf39e909f8664e0f120550cee
Pawel Cetnarowski <student.pawel2000@gmail.com>
2021-11-28 15:33:29 +0100

    Wersja ostateczna

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>  
 #include <math.h>
+#include "kwadratowa.h"
+#include "liniowa.h"
+#include "kanoniczna.h"
 using namespace std;
 
  float a,b,c,d,x,x1,x2; 
@@ -7,49 +10,33 @@ using namespace std;
  int main ()
 
 { 
+    int wybor;
+    cout << "MENU: " << endl;
+    cout << "1. Funkcja liniowa"<<endl;
+    cout << "2. Funkcja kwadratowa"<<endl;
+    cout << "3. Funkcja kwadratowa postac kanoniczna"<<endl<<endl;
+    cout << "Wybor: ";
+    cin>>wybor;
+    
+    if(wybor <1 && wybor > 3) cout<<endl<<"Wprowadz numer z listy",cin>>wybor;
+	
+    
+    
+    switch(wybor){
+    	case 1:{
+    		liniowa();
+ 
+			break;
+		}
+    	case 2:{
+		kwadratowa();
 
-    cout<<"Program zwraca miejsce zerowe funkcji kwadratowej postaci ax^2+bx+c";                                                                                                                // Tekst wyœwietlany na konsoli.
-
-    cout<<"\n Podaj wspolczynnik kierunkowy prostej a =/0"<<endl; cin>>a;   
-     if(a==0){
-	 
-    cout<<"Niema takiej funkcji kwadratowej  ";
-	exit(0); 
-	}//sprawdzanie warunku a
-    cout<<"Podaj wspolczynnik b "<<endl; cin>>b;
-    cout<<"Podaj wspolczynnik c "<<endl; cin>>c;
-
-
-    d=(b*b)-(4*a*c); 
-
-    if(d>0) 
-
-    {
-
-        x1=(-b-sqrt(d))/(2*a);  // Wyznaczenie miejsca zerowego x1.
-
-        x2=(-b+sqrt(d))/(2*a);  // Wyznaczenie miejsca zerowego x2.
-
-        cout<<"Wynik to :\nx1 = "<<x1<<"\nx2 = "<<x2; // Wypisanie wyniku na konsoli.
-
-    }
-
-    if(d==0)  
-
-    {
-
-        x=-b/(2*a);  // Okreœlenie miejsca zerowego.
-
-        cout<<"Wynik to : \n x = "<<x;
-
-    }
-
-     if(d<0)  // Jeœli delta mniejsza od "0".
-
-    {      
-
-    cout<<"Brak rozwiazan";  // Wyœwietlenie komentarza.
-
-    }
+    	break;
+		}
+		case 3:{
+			kanoniczna();
+			break;
+		}
+	}
 
 }
